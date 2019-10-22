2224cc35ddd70cec8606013ff4062a66fa240022
Jente Vets <jente.vets@brusselsairlines.com>
2019-10-22 17:27:41 +0200

    Base implementation of scraping the first table

---
+++ Kzvb.DataScraper/Kzvb.DataScraper/Models/GameResultModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Kzvb.DataScraper.Models
+{
+	public class GameResultModel
+	{
+		public string Wdn { get; set; }
+		public string Division { get; set; }
+		public string Date { get; set; }
+		public string Home { get; set; }
+		public string Visitors { get; set; }
+		public string Score { get; set; }
+	}
+}
